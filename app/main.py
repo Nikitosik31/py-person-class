da06bff00353812de541bd8247a89837ba1467a5
Nik <nikita.pitushkin31@mail.com>
2025-10-01 12:21:27 +0300

    Bugs fixed

--- app/main.py
+++ app/main.py
@@ -8,24 +8,30 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    persons = []
-    for person_dict in people:
-        name = person_dict["name"]
-        age = person_dict["age"]
-        persons.append(Person(name, age))
+def create_person_list(people: list[dict]) -> list[Person]:
+    Person.people.clear()
+
+    persons = [
+        Person(
+            p.get("name"),
+            p.get("age")
+        )
+        for p in people
+    ]
 
     for person_dict in people:
         name = person_dict["name"]
         current = Person.people[name]
 
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            partner = Person.people[person_dict["wife"]]
+        wife_name = person_dict.get("wife")
+        if wife_name:
+            partner = Person.people[wife_name]
             current.wife = partner
             partner.husband = current
 
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            partner = Person.people[person_dict["husband"]]
+        husband_name = person_dict.get("husband")
+        if husband_name:
+            partner = Person.people[husband_name]
             current.husband = partner
             partner.wife = current
 
