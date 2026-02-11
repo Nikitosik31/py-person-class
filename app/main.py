c866915ccabbd184774ca091d5a8f157d995e8c5
Nik <nikita.pitushkin31@mail.com>
2026-02-11 17:59:50 +0200

    add fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for human in people:
+        Person(human["name"], human["age"])
+
+    person_list = []
+    for human in people:
+        person = Person.people[human["name"]]
+
+        if human["wife"]:
+            person.wife = Person.people[human["wife"]]
+
+        if human["husband"]:
+            person.husband = Person.people[human["husband"]]
+
+        person_list.append(person)
+
+    return person_list
